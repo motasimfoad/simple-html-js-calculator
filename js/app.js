b518446c6873e6aeaefffbb3368468b7175db755
Motasim Foad <motasimfoad@gmail.com>
2020-11-10 04:55:59 +0600

    changed layout to non tabular view

--- js/app.js
+++ js/app.js
@@ -1,4 +1,8 @@
 window.onload = function (){
    var year = new Date().getFullYear();
-   document.getElementById("footer_year").innerHTML = year
+   document.getElementById("footer_year").innerHTML = year;
+   function display(data){
+      document.getElementById("display").value+=data+" ";
    };
+   };
+
