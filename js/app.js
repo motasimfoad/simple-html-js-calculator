d5ac3a649267b6e96e57560b5312c4a2e35a7542
Motasim Foad <motasimfoad@gmail.com>
2020-11-10 19:28:23 +0600

    Added button input to numbers to display

--- js/app.js
+++ js/app.js
@@ -1,8 +1,17 @@
+var display_value = 0;
+
 window.onload = function (){
    var year = new Date().getFullYear();
    document.getElementById("footer_year").innerHTML = year;
-   function display(data){
-      document.getElementById("display").value+=data+" ";
-   };
+   document.getElementById("display_number").innerHTML=display_value;
+};
+
+function display(data){
+      display_value = data; 
+      document.getElementById("display_number").innerHTML+=display_value;
+      console.log(display_value);
    };
 
+function clear(){
+   display_value = ""
+}
