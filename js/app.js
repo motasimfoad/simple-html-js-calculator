62b545579b1cbec1bc19c7f5190acabdef9c5f46
Motasim Foad <motasimfoad@gmail.com>
2020-11-10 19:44:53 +0600

    Clear function added, display clear

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-var display_value = 0;
+var display_value = "";
 
 window.onload = function (){
    var year = new Date().getFullYear();
@@ -9,9 +9,10 @@ window.onload = function (){
 function display(data){
       display_value = data; 
       document.getElementById("display_number").innerHTML+=display_value;
-      console.log(display_value);
-   };
+      
+};
 
-function clear(){
-   display_value = ""
-}
+function clr(){
+   display_value = " "
+   document.getElementById("display_number").innerHTML=display_value;
+};
