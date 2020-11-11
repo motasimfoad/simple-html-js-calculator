6695e5fa2b797c25a20236bdec28bd643299e31d
Motasim Foad <motasimfoad@gmail.com>
2020-11-11 21:12:05 +0600

    added backspace with proper fnctions

--- js/app.js
+++ js/app.js
@@ -2,47 +2,40 @@ var display_value = "";
 var display_string;
 var splited_string;
 var result;
+var flag = false;
 
 window.onload = function (){
    var year = new Date().getFullYear();
    document.getElementById("footer_year").innerHTML = year;
-   document.getElementById("display_number").innerHTML=display_value;
+   document.getElementById("display_number").innerHTML = display_value;
 };
 
 function display(data){
       display_value = data; 
-      document.getElementById("display_number").innerHTML+=display_value;
-      display_string = document.getElementById("display_number").innerText;  
+      if (flag==false) {
+         document.getElementById("display_number").innerHTML+=display_value;
+         display_string = document.getElementById("display_number").innerText;  
+      }
+      else{
+         document.getElementById("display_number").innerHTML = display_value;
+         display_string = document.getElementById("display_number").innerText;
+         flag = false; 
+      }
 };
 
 function clr(){
    display_value = " "
-   document.getElementById("display_number").innerHTML=display_value;
+   document.getElementById("display_number").innerHTML = display_value;
 };
 
-function string_to_array(){
-   splited_string = display_string.split("");
-   // var x;
-   // splited_string.map(item => {
-   //    switch (item) {
-   //       case item<10:
-   //          x = item;
-   //          return console.log(x);
-
-   //       case item<10:
-   //          x = item;
-           
-   //       case item<10:
-   //          x = item;
-         
-   //       default:
-   //          break;
-   //    }
-   // })
+function backspace() {
+      flag = true;
+      display_value = display_string.slice(0, -1);
+      document.getElementById("display_number").innerHTML = display_value;
+      display(display_value)
 }
 
 function solve(){
-   //   string_to_array();
    result = eval(display_string);
    if (result!=undefined) {
       document.getElementById("display_number").innerHTML=result;
